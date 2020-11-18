68553f4ba3f93fa42ee8a61e240a0f38070e08e7
Nilesh Solanki <pujara201@gmail.com>
2020-11-18 21:31:39 +0530

    move .conf creation

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@
 wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
 unzip -q rclone-v1.52.3-linux-amd64.zip
 export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
+
+if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
+	echo "Rclone config detected"
+	echo "[DRIVE]" > rclone.conf
+    mkdir -p /app/.config/rclone
+	echo "$(echo $RCLONE_CONFIG_BASE64|base64 -d)" >> /app/.config/rclone/rclone.conf
+fi
