2485707462273c1da8495cb52fcc0f17ca1386aa
Nilesh Solanki <pujara201@gmail.com>
2020-11-18 21:36:33 +0530

    dffs

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
 unzip -q rclone-v1.52.3-linux-amd64.zip
 export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
 
+echo "port is $PORT"
+
 if [[ -n $RCLONE_CONFIG_BASE64 ]]; then
 	echo "Rclone config detected"
 	echo "[DRIVE]" > rclone.conf
